774956a0ea2738fdd1d0ac2a943c7333ee75a5a4
andonialvarez <andonialvarez3@gmail.com>
2024-05-06 13:59:54 +0200

    Version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { Slider1Component } from './slider1/slider1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, PresentationComponent, Slider1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'andoni-portfolio';
+ 
 }
